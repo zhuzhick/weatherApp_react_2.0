12f64bb6e7a2d386f3040b3592803962190b6612
zhuzhick <a.holtceva@gmail.com>
2022-09-19 18:43:23 +0300

    GitHub link added

--- weather_app/src/Developer.js
+++ weather_app/src/Developer.js
@@ -7,7 +7,7 @@ export default function Developer() {
 			<div className="col-md-12">
 				<hr className="hide" />
 				<h5 className="developer">
-					<a href="https://github.com/zhuzhick/weatherApp_react">
+					<a href="https://github.com/zhuzhick/weatherApp_react_2.0">
 						Open-source code
 					</a>
 					by A.Holtseva
